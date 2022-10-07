d950280aa7bc540ec03579548b184f6403bf268c
JaneDann <97164213+JaneDann@users.noreply.github.com>
2022-10-07 16:15:51 +0500

    fix13

---
+++ telegram.php
@@ -0,0 +1,42 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (!empty($_POST['name'])){
+  if (isset($_POST['name'])) {
+    if (!empty($_POST['name'])){
+      $title = strip_tags($_POST['name']);
+      $titleFieldset = "Имя: ";
+    }
+  }
+}
+if (!empty($_POST['contact'])){
+  if (isset($_POST['contact'])) {
+    if (!empty($_POST['contact'])){
+      $email = strip_tags($_POST['contact']);
+      $emailFieldset = "Контакт: ";
+    }
+  }
+}
+if (!empty($_POST['username'])){
+  if (isset($_POST['username'])) {
+    if (!empty($_POST['username'])){
+      $content = strip_tags($_POST['username']);
+      $contentFieldset = "Username: ";
+    }
+  }
+}
+
+
+$token = "5305936319:AAGZi0Z6q50wp5K-4wyfb71TkJUW5Q76c0E";
+$chat_id = "-1001771111758";
+$arr = array(
+  $titleFieldset => $title,
+  $emailFieldset => $email,
+  $contentFieldset => $content
+);
+$txt = '';
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+}
+?>
